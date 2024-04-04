14a0ac94c8cf9105aec279f88a54d641dce41624
Andres Felipe Campaz <dtmo96@outlook.com>
2024-04-04 00:33:04 -0500

    Initial commit

---
+++ src/test/java/com/wallet/recarga/RecargaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.recarga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecargaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
